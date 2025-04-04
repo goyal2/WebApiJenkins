22c37ed059eacf61a55e2cff19564d74b82e6cb0
SomyaGoyal <115345500+goyal2@users.noreply.github.com>
2025-04-04 16:04:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,42 @@
-//Configuration the azure Provider
 provider "azurerm" {
+  features {}
 
-  features { 
-  }
-  client_id = var.client.id
-  client_secret = var.client_secret
-  teanent_id = var.tenant_id
-  subscritption_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
 
-//Create a resource group with a new name
+# Resource Group
 resource "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
+  name     = var.resource_group_name
   location = var.location
 }
 
+# App Service Plan
 resource "azurerm_app_service_plan" "plan" {
-  name = var.app_service_plan_name
-  location = azurerm_resource_group.rg.location
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
- 
+
   sku {
     tier = "Basic"
     size = "B1"
   }
 }
 
+# App Service (Web App)
 resource "azurerm_app_service" "app" {
-  name = var.app_service_name
-  location =  azurerm_resource_group.rg.location
+  name                = var.app_service_name
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.plan.id
 
   site_config {
-    dotnet_framework_version =  "v6.0"
+    always_on = true
   }
+
   app_settings = {
-    WEBSITE_RUN_FRON_PACKAGE = "1"
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
-} 
+}
